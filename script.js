eaeeddf177d028a5fc51ca2f86ce6babbe07fb4b
Christian Nirschl <christiannirschl6@gmail.com>
2021-10-10 18:11:20 -0700

    Updated page with projects

---
+++ script.js
@@ -0,0 +1,21 @@
+// var imgOne = document.getElementById("imgone");
+
+
+// element.addEventListener("click",function(){
+// alert("clicked");
+// })
+
+
+    // $(document).ready(function() {
+    //     $('img.thumbnail').click(function() {
+    //         window.location.href = this.id + '.html';
+    //     });
+    // });
+
+//     var images = document.getElementById("imgone");
+// for(var i = 0; i < images.length; i++) {
+//     var image = images[i];
+//     image.onclick = function(event) {
+//          window.location.href =  'https://cpysden-coder.github.io/wk4-project-html/';
+//     };
+// }
